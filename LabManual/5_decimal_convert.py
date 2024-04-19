0bef97d16cdded95c2a7439d5cebb5e2074e30f7
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-04-19 15:18:14 +0530

    Create 5_decimal_convert.py

---
+++ LabManual/5_decimal_convert.py
@@ -0,0 +1,8 @@
+# Q5. Write a python program to Convert Decimal to Binary, Octal and Hexadecimal 
+
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
