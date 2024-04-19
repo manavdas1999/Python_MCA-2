590e077b37c6e577c9056128a4f1320ed1719ee9
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-04-19 15:28:27 +0530

    Create 7_char_to_ascii.py

---
+++ LabManual/7_char_to_ascii.py
@@ -0,0 +1,7 @@
+# Q7. Write a python program to Find ASCII Value of Character. 
+
+userInput = input("Enter your Character:")
+
+ascii_value = ord(userInput)
+
+print(f"ASCII value of the Character {userInput} is: {ascii_value}")
