1ab3ba1e340ab433ad6e7fb09cff4fc69f7307e7
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-04-19 15:06:17 +0530

    Create 3_arithmetic.py

---
+++ LabManual/3_arithmetic.py
@@ -0,0 +1,14 @@
+#Q3. Write a python Program for addition, Subtraction, multiplication, division 
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+
+print(f"Addition of {num1} and {num2}:", add)
+print(f"Subtraction of {num1} and {num2}:", subtract)
+print(f"Multiplication of {num1} and {num2}:", multiply)
+print(f"Division of {num1} and {num2}:", divide)
